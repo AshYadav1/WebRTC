de45cd47b0d10e493b91346264d064229071dca1
Ashish Yadav <ashish.yadav@techqware.com>
2026-04-03 01:24:37 +0400

    Compiled WebRTC framework version.
    milestone: 146
    WebRTC: 7680

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "WebRTC",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "WebRTC", targets: ["WebRTC"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "WebRTC",
+            path: "WebRTC.xcframework"
+        )
+    ]
+)
